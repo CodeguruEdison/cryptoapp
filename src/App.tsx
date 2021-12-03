18bb6fb0a45f3311a6d45bdc35e14a02737b3654
Ayaskanta Samal <ayaskantasamal@Ayaskantas-MacBook-Pro.local>
2021-12-02 20:31:52 -0500

    [12-02-2021] - created the dummy components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import './App.css';
-import { Route,Link } from 'react-router-dom';
-import {Layout,Typography,Space } from 'antd';
-import {NavBar} from './components'
+import { Routes,Route,Link } from 'react-router-dom';
+import {Layout,Typography,Space, } from 'antd';
+import {NavBar,HomePage,Exchanges,CryptoCurrencies,CryptoDetail,News} from './components'
 const App =()=>{
    return (
    <div className="app">
        <div className="navbar">
         <NavBar/>
        </div>
-       < div className="main"></div>
+       < div className="main">
+         <Layout>
+            <div className="routes">
+              <Routes>
+                  <Route  path ="/">
+                      <HomePage/>
+                  </Route>
+                  <Route  path ="/exchanges">
+                      <Exchanges/>
+                  </Route>
+                  <Route  path ="/cryptocurrencies">
+                      <CryptoCurrencies/>
+                  </Route>
+                  <Route  path ="/crypto/:coinid">
+                      <CryptoDetail/>
+                  </Route>
+                  <Route  path ="/news">
+                      <News/>
+                  </Route>
+              </Routes>
+            </div>
+         </Layout>
+       </div>
        <div className="footer">
 
        </div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
 export {default as NavBar} from './NavBar/NavBar'
+export {default as HomePage} from './HomePage/HomePage'
+export {default as Exchanges} from './Exchanges/Exchanges';
+export {default as CryptoCurrencies} from './CryptoCurrencies/CryptoCurrencies';
+export {default as CryptoDetail} from './CryptoCurrencies/CryptoDetail';
