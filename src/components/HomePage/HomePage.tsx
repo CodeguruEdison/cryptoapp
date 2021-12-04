bb8624a39daebb93bc7fdca62e03c6f1605c202a
Ayaskanta Samal <ayaskantasamal@Ayaskantas-MacBook-Pro.local>
2021-12-04 08:44:43 -0500

    [12-04-2021] - Crypto currency api method added

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import millify from 'millify';
-import { Link } from 'react-router-dom';
 import { Col, Row, Statistic, Typography } from 'antd';
-
+import {useGetCryptosQuery} from  '../../services/CryptoApi';
 const { Title } = Typography;
 const HomePage = () => {
+    const {data,isFetching} = useGetCryptosQuery({});
+    const globalStats = data?.data?.stats ;
+    if(isFetching) return <>'Loading...'</>;
     return (
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
             <Row>
-                <Col span={12}><Statistic title="Total Cryptocurrencies" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
                 <Col span={12}><Statistic title="Total Exchanges" value="5" /></Col>
                 <Col span={12}> <Statistic title="Total Market Cap" value="5" /></Col>
                 <Col span={12}><Statistic title="Total 24h Volume" value="5" /></Col>
---
+++ src/services/CryptoApi.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'd5b765e8c3mshf99b49e9d38c8adp11b6bejsndc5038d81b92'
+}
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url: string) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins'),
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery
+}= cryptoApi ;
